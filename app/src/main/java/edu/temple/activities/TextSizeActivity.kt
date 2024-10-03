ae3a34dbc7d3d74287e961d4fe3486cf58c671c7
Mitchell Fishman <tuq42483@temple.edu>
2024-10-03 15:19:57 -0400

    Final commit, completed all steps

--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+const val RESULT_KEY = "message"
 
 class TextSizeActivity : AppCompatActivity() {
 
@@ -23,8 +25,9 @@ class TextSizeActivity : AppCompatActivity() {
         with (findViewById(R.id.textSizeSelectorRecyclerView) as RecyclerView) {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
-            adapter = TextSizeAdapter(textSizes){
-
+            adapter = TextSizeAdapter(textSizes) {
+                setResult(RESULT_OK, Intent().putExtra(RESULT_KEY, it.toFloat()))
+                finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
